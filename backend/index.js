1ab215459ca546220c01e14b6c5e6b3d335344ef
Shamik Mandal <shamikmandal170@gmail.com>
2026-02-18 19:38:33 +0530

    backend: setup express server entry point and routes with added mongodb connection configuration

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+// import expenseRoutes from "./routes/expenseRoutes.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// app.use("/api/expenses", expenseRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
